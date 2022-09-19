81910ea29fc07420b0ac693393bd580e184f0e44
YarlinVargas <yxvargash@unal.edu.co>
2022-09-19 16:49:27 -0500

    ok

---
+++ ECommerceApp/ECommerceApp/ECommerceApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ECommerceApp.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ECommerceApp
+{
+    public partial class App : Application
+    {
+        public static NavigationPage Navigator { get; internal set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MasterPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
